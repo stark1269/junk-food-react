8815bd07f93a05fd6cafdccfaad88f4700bbce84
stark1269 <112755303+stark1269@users.noreply.github.com>
2023-06-19 17:19:03 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -3,15 +3,13 @@ import { Header, Wrapp, Link, LinkIcon } from "./AppBar.styled";
 import { TbShoppingCartOff, TbShoppingCartPlus } from 'react-icons/tb';
 
 export const AppBar = ({ shopCart }) => {
-  const shopCartLength = shopCart.length;
-
   return (
     <Header>
       <Wrapp>
         <Link to='/rest'>Restaurants</Link>
         {!shopCart.length ?
-          <LinkIcon shopCartLength={shopCartLength} to='/rest'><TbShoppingCartOff /></LinkIcon> :
-          <LinkIcon shopCartLength={shopCartLength} to='/shop-cart'><TbShoppingCartPlus /></LinkIcon>}
+          <LinkIcon shopCartLength={shopCart.length} to='/rest'><TbShoppingCartOff /></LinkIcon> :
+          <LinkIcon shopCartLength={shopCart.length} to='/shop-cart'><TbShoppingCartPlus /></LinkIcon>}
       </Wrapp>
       <UserMenu />
     </Header>
